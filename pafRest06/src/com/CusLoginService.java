9239a99e7e7989a8286cc29d29c9c753395ac848
Melina <68732960+melinawijayarathna77@users.noreply.github.com>
2021-04-21 19:21:40 +0530

    Customer login completed

--- pafRest06/src/com/CusLoginService.java
+++ pafRest06/src/com/CusLoginService.java
@@ -1,5 +1,31 @@
 package com;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
+import model.CusLogin;
+
+
+@Path("/logins")
 public class CusLoginService {
+	
+CusLogin loginobj= new  CusLogin();
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String Login(@FormParam("cname") String cname,@FormParam("password") String password)
+	{
+		
+		boolean output = loginobj.Login(cname, password);
+		if(output) {
+			return "Login success";
+		}
+		else {
+			return "Login Failed";
+		}
+		
+	}
 
 }
--- pafRest06/src/model/CusLogin.java
+++ pafRest06/src/model/CusLogin.java
@@ -1,5 +1,58 @@
 package model;
 
-public class CusLogin {
+import java.sql.*;
 
+public class CusLogin {
+	//A common method to connect to the DB
+	private Connection connect() 
+	 { 
+		Connection con = null; 
+		try
+		{ 
+			Class.forName("com.mysql.jdbc.Driver"); 
+	 
+			//Provide the correct details: DBServer/DBName, username, password 
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/customer", "root", ""); 
+		} 
+		
+		catch (Exception e) 
+		{e.printStackTrace();} 
+		return con; 
+	 } 
+
+	public boolean Login(String cname,String password) {
+		String output = "";
+		Connection con=null;
+	   try{           
+		    con = connect();
+			if (con == null) {
+				output= "Error while connecting to the database for updating.";
+			}
+			
+	       PreparedStatement pst = con.prepareStatement("Select * from account where cname=? and password=?");
+	       pst.setString(1, cname); 
+	       pst.setString(2, password);
+	       ResultSet rs = pst.executeQuery();
+	      boolean apple=rs.next();
+	      con.close();
+	       if(apple) {
+	    	   
+	    	   System.out.println(cname);
+	       
+	           return true;  
+	       }
+	       else {
+	    	   return false;  
+	      
+	       }
+	      
+	   }
+	   catch(Exception e){
+	       e.printStackTrace();
+	       return false;
+	   } 
+	  
+	}
+	
+	
 }
